963a8f965fe46daffceea4d590b87442e6f2ac25
alfrch <alifrach@gmail.com>
2026-03-02 21:29:22 +0700

    separate game list and game detail mapper

--- Sources/Game/Util/GameTransformer.swift
+++ Sources/Game/Util/GameTransformer.swift
@@ -2,49 +2,47 @@
 //  GameTransformer.swift
 //  Game
 //
-//  Created by Alif Rachmawan on 26/02/26.
+//  Created by Alif Rachmawan on 02/03/26.
 //
 
+import Foundation
 import Core
 
 public struct GameTransformer: Mapper {
+  
   public typealias Request = Any
-  public typealias Response = [GameResponse]
-  public typealias Entity = [GameModuleEntity]
-  public typealias Domain = [GameModel]
+  public typealias Response = GameResponse
+  public typealias Entity = GameModuleEntity
+  public typealias Domain = GameModel
   
   public init() {}
   
-  public func transformResponseToEntity(request: Any?, response: [GameResponse]) -> [GameModuleEntity] {
-    return response.map { result in
-      let gameEntity = GameModuleEntity()
-      gameEntity.id = "\(result.id ?? 0)"
-      gameEntity.title = result.title ?? "Unknown Title"
-      gameEntity.price = result.worth ?? "N/A"
-      gameEntity.thumbnail = result.thumbnail ?? ""
-      gameEntity.desc = result.description ?? "No description available"
-      gameEntity.type = result.type ?? "Unknown"
-      
-      let platformArray = result.platforms?.components(separatedBy: ", ") ?? []
-      gameEntity.platforms.removeAll()
-      gameEntity.platforms.append(objectsIn: platformArray)
-      return gameEntity
-    }
+  public func transformResponseToEntity(request: Request?, response: GameResponse) -> GameModuleEntity {
+    let gameEntity = GameModuleEntity()
+    gameEntity.id = UUID().uuidString
+    gameEntity.title = "Unknown Title"
+    gameEntity.price = "N/A"
+    gameEntity.thumbnail = ""
+    gameEntity.desc = "No description available"
+    gameEntity.type = "Unknown"
+    
+    let platformArray = [""]
+    gameEntity.platforms.removeAll()
+    gameEntity.platforms.append(objectsIn: platformArray)
+    return gameEntity
   }
   
-  public func transformEntityToDomain(entity: [GameModuleEntity]) -> [GameModel] {
-    return entity.map { result in
-      return GameModel(
-        id: Int(result.id) ?? 0,
-        title: result.title,
-        price: result.price,
-        thumbnail: result.thumbnail,
-        description: result.desc,
-        type: result.type,
-        platforms: Array(result.platforms),
-        url: result.url,
-        favorite: result.favorite
-      )
-    }
+  public func transformEntityToDomain(entity: GameModuleEntity) -> GameModel {
+    return GameModel(
+      id: Int(entity.id) ?? 0,
+      title: entity.title,
+      price: entity.price,
+      thumbnail: entity.thumbnail,
+      description: entity.desc,
+      type: entity.type,
+      platforms: Array(entity.platforms),
+      url: entity.url,
+      favorite: entity.favorite
+    )
   }
 }
